81f4b15e73b5804ec4ea375ee458052e508337bb
Vivek Vadgama <vivekvadgama34@gmail.com>
2023-08-19 22:43:56 +0530

    SVG Warrning Resolve

--- src/assets/icons/menu-icon.js
+++ src/assets/icons/menu-icon.js
@@ -7,20 +7,20 @@ export const MenuIcon = () => (
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M29 2.11492H0V0.528809H29V2.11492Z"
       fill="black"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M29 23.2629H0V21.6768H29V23.2629Z"
       fill="black"
     />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M29 12.6889H0V11.1028H29V12.6889Z"
       fill="black"
     />
